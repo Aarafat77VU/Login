561c3db338723134fec27f016342b32bbc2fc7cb
Aarafat77VU <aarafatvu77@gmail.com>
2019-03-23 00:23:06 +0600

    Initial commit

---
+++ Login/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using Login.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private IMSEntities db = new IMSEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(Login.Models.UserDetail userModel)
+        {
+            using (IMSEntities db = new IMSEntities())
+            {
+                var userDetails = db.UserDetails.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong Username or Password";
+                    return View("Index", userModel);
+                }
+
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+                    Session["UserName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+
+
+
+            }
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "UserID,Name,UserName,Email,Password")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserDetails.Add(userDetail);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(userDetail);
+        }
+    }
+}
